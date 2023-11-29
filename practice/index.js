9d891d2c87ea5e455084f43bf2cd927ef98b0451
Louis Joseph <louisjosephleadway@gmail.com>
2023-11-29 13:02:34 +0100

    ready for deployment

--- practice/index.js
+++ practice/index.js
@@ -2,6 +2,11 @@
 
 const recipent = "James"
 // refactor the email string to use template strings
+// create a new variable, sender and set its value to your name
+const sender = "Louis Joseph"
 
-const email = `Hey ${recipent} ! How is it going? Cheers Per`
+const email = `
+Hey ${recipent} ! 
+How is it going? 
+Cheers ${sender}`
 alert(email)
